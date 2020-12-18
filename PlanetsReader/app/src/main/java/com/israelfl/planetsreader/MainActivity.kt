7f3d65a7976f75a86d0a6e096b892a682068a76d
Israel Flores <israel.flores@gc.com>
2020-12-17 20:08:56 -0500

    Reset

--- PlanetsReader/app/src/main/java/com/israelfl/planetsreader/MainActivity.kt
+++ PlanetsReader/app/src/main/java/com/israelfl/planetsreader/MainActivity.kt
@@ -9,23 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        contentResolver.query(
-            Uri.parse("content://com.israelfl.planets.PlanetProvider"),
-            arrayOf("planets"),
-            null,
-            null,
-            null
-        )?.apply {
-            if (moveToFirst()) {
-                do {
-                    val planets = getString(0)
-                    findViewById<TextView>(R.id.tv_json).apply {
-                        text = planets
-                    }
-                } while (moveToNext())
-            }
-            close()
-        }
     }
 }
